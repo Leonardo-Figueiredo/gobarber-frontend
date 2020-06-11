f568adbaba37ccbf04a7bf57d3bab0d801decc93
Leonardo-Figueiredo <leo.nardorf22@gmail.com>
2020-06-11 20:15:09 -0300

    Context and Authorization concepts in frontend

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,56 @@
+import React, { createContext, useCallback, useState } from 'react';
+
+import api from '../services/apiClient';
+
+interface AuthState {
+  token: string;
+  user: object;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: object;
+  signIn(credentials: SignInCredentials): Promise<void>;
+}
+
+export const AuthContext = createContext<AuthContextData>(
+  {} as AuthContextData,
+);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+
+    return token && user
+      ? { token, user: JSON.parse(user) }
+      : ({} as AuthState);
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post<{ token: string; user: object }>(
+      'sessions',
+      {
+        email,
+        password,
+      },
+    );
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    setData({ token, user });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
